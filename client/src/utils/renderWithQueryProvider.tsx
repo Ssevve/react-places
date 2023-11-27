fbfe90868f3925656f58dc48a035bf49e29da194
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-27 19:13:38 +0100

    Move queryClient instance outside custom render function

--- client/src/utils/renderWithQueryProvider.tsx
+++ client/src/utils/renderWithQueryProvider.tsx
@@ -1,7 +1,7 @@
 import { render } from '@testing-library/react';
 import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 
+const queryClient = new QueryClient();
 export const renderWithQueryProvider = (component: JSX.Element) => {
-  const queryClient = new QueryClient();
   render(<QueryClientProvider client={queryClient}>{component}</QueryClientProvider>);
 };
