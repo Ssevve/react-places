65217924d25f97427472acaa0611424b7c757453
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-28 20:42:03 +0100

    Remove DataBox component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,34 @@
 import { styled } from '@mui/material';
 import { Map } from '@/components/Map';
-import { DataBox } from '@/components/DataBox';
+import Paper from '@mui/material/Paper';
+import { BusinessList } from '@/components/BusinessList';
 
-const ContentWrapper = styled('div')({
+const AppWrapper = styled('div')({
   display: 'flex',
   position: 'relative',
 });
 
+export const ContentWrapper = styled(Paper)(({ theme }) => ({
+  width: '100%',
+  height: '100vh',
+  position: 'absolute',
+  [`@media (min-width: ${theme.breakpoints.values.sm}px)`]: {
+    height: '700px',
+    width: '400px',
+    position: 'absolute',
+    left: '1rem',
+    top: '1rem',
+    overflow: 'auto',
+  },
+}));
+
 export function App() {
   return (
-    <ContentWrapper>
+    <AppWrapper>
       <Map />
-      <DataBox />
-    </ContentWrapper>
+      <ContentWrapper elevation={6}>
+        <BusinessList />
+      </ContentWrapper>
+    </AppWrapper>
   );
 }
