5c8939cb4084a42235a12e09f87d6ddb31669372
Seweryn Świrgoń <ssevve@gmail.com>
2023-11-30 00:05:00 +0100

    Change function name

--- client/src/features/businesses/components/BusinessYelpRating/BusinessYelpRating.tsx
+++ client/src/features/businesses/components/BusinessYelpRating/BusinessYelpRating.tsx
@@ -1,13 +1,13 @@
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-function getImageName(rating: number) {
+const getYelpRatingImageName = (rating: number) => {
   const flatRating = Math.floor(rating);
   const ratingString = rating === 0.5 ? '' : `_${flatRating}`;
   return Number.isInteger(rating)
     ? `regular${ratingString}.png`
     : `regular${ratingString}_half.png`;
-}
+};
 
 interface BusinessYelpRatingProps {
   rating: number;
@@ -18,7 +18,7 @@ interface BusinessYelpRatingProps {
 export function BusinessYelpRating({ rating, reviewCount }: BusinessYelpRatingProps) {
   return (
     <Box display="flex" gap="0.5rem" alignItems="center">
-      <img width={102} src={`src/assets/yelp-stars/${getImageName(rating)}`} />
+      <img width={102} src={`src/assets/yelp-stars/${getYelpRatingImageName(rating)}`} />
       <Typography component="span" fontSize={14} fontWeight="700">
         {rating}
       </Typography>
