f1be224b237547f411d5ec5881959f58f1218356
Seweryn Świrgoń <ssevve@gmail.com>
2023-12-03 23:27:56 +0100

    Add BusinessList tests in App tests

--- client/src/App.test.tsx
+++ client/src/App.test.tsx
@@ -1,10 +1,34 @@
 import { App } from '@/App';
+import { server } from '@/__mocks__/server';
 import { renderWithQueryProvider } from '@/utils/renderWithQueryProvider';
-import { screen } from '@testing-library/react';
+import { screen, waitFor } from '@testing-library/react';
+import { HttpResponse, http } from 'msw';
 
 describe('App', () => {
   it('should render <Map /> component', () => {
     renderWithQueryProvider(<App />);
-    screen.getByTestId('map');
+    expect(screen.getByTestId('map')).toBeInTheDocument();
+  });
+
+  it('should render <BusinessList /> component failed businesses fetch', async () => {
+    renderWithQueryProvider(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByRole('list', { name: /business list/i })).toBeInTheDocument();
+    });
+
+    expect(screen.queryByText("Couldn't load businesses")).not.toBeInTheDocument();
+  });
+
+  it('should render <BusinessListErrorFallback /> on failed businesses fetch', async () => {
+    vi.spyOn(console, 'error').mockImplementation(() => {});
+    server.use(http.get('http://localhost:5000/yelp', HttpResponse.error));
+    renderWithQueryProvider(<App />);
+
+    await waitFor(() => {
+      expect(screen.getByText("Couldn't load businesses")).toBeInTheDocument();
+    });
+
+    expect(screen.queryByRole('list', { name: /business list/i })).not.toBeInTheDocument();
   });
 });
