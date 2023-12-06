84565c224d36166f6ef4484d52c733a8c00aca53
Seweryn Świrgoń <ssevve@gmail.com>
2023-12-06 17:42:43 +0100

    Add InteractiveMarker component

---
+++ client/src/components/InteractiveMarker.tsx
@@ -0,0 +1,43 @@
+import { styled } from '@mui/material';
+import L from 'leaflet';
+import { ForwardedRef, forwardRef, useImperativeHandle, useMemo, useState } from 'react';
+import { createPortal } from 'react-dom';
+import { Marker, MarkerProps } from 'react-leaflet';
+
+interface InteractiveMarkerProps extends MarkerProps {
+  iconOptions?: L.DivIconOptions;
+}
+
+const StyledMarker = styled(Marker)({
+  position: 'relative',
+});
+
+export const InteractiveMarker = forwardRef(
+  (
+    { children, iconOptions, ...rest }: InteractiveMarkerProps,
+    forwardedRef: ForwardedRef<L.Marker | undefined>,
+  ) => {
+    const [markerInstance, setMarkerInstance] = useState<L.Marker | undefined>();
+
+    useImperativeHandle(forwardedRef, () => markerInstance);
+
+    return (
+      <>
+        {useMemo(
+          () => (
+            <StyledMarker
+              {...rest}
+              ref={(node) => setMarkerInstance(node || undefined)}
+              icon={L.divIcon({ ...iconOptions, className: '', iconSize: undefined })}
+            />
+          ),
+          // eslint-disable-next-line react-hooks/exhaustive-deps
+          [],
+        )}
+        {markerInstance && markerInstance.getElement()
+          ? createPortal(children, markerInstance.getElement()!)
+          : null}
+      </>
+    );
+  },
+);
