9c71beb669fe5e37db8a7c1c293b5ac8fd6bfae3
Seweryn Świrgoń <ssevve@gmail.com>
2023-12-23 10:21:49 +0100

    Display business icon in BusinessListErrorFallback

--- client/src/features/businesses/components/BusinessListErrorFallback/BusinessListErrorFallback.tsx
+++ client/src/features/businesses/components/BusinessListErrorFallback/BusinessListErrorFallback.tsx
@@ -1,3 +1,4 @@
+import DomainDisabledIcon from '@mui/icons-material/DomainDisabled';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
@@ -8,7 +9,8 @@ interface BusinessListFallbackProps {
 
 export function BusinessListErrorFallback({ resetErrorBoundary }: BusinessListFallbackProps) {
   return (
-    <Box role="alert" display="grid" justifyContent="center" gap={1} padding={2}>
+    <Box role="alert" display="grid" justifyContent="center" gap={2} padding={2} textAlign="center">
+      <DomainDisabledIcon sx={{ fontSize: 80, mx: 'auto', opacity: 0.4 }} />
       <Typography component="p">Couldn't load businesses</Typography>
       <Button variant="contained" size="small" onClick={resetErrorBoundary}>
         Try again
