6f11cd85e509a605e0683536c2c655180cff4991
Seweryn Świrgoń <ssevve@gmail.com>
2023-12-28 14:37:54 +0100

    Remove console warning spy from BusinessCardCategories tests

--- client/src/features/businesses/components/BusinessCardCategories/BusinessCardCategories.test.tsx
+++ client/src/features/businesses/components/BusinessCardCategories/BusinessCardCategories.test.tsx
@@ -15,8 +15,6 @@ const categories: Category[] = [
 
 describe('BusinessCardCategories', () => {
   it('should render all provided categories', () => {
-    // Silence the duplicate keys warning (caused by mocking library)
-    vi.spyOn(console, 'warn').mockImplementation(() => {});
     render(<BusinessCardCategories categories={categories} />);
     categories.forEach(({ title }) => {
       expect(screen.getByText(title)).toBeInTheDocument();
