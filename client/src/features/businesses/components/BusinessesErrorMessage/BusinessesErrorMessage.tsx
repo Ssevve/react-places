1f14a7cffd75a9b50fe8263bd12f7084253bb6f5
Seweryn Świrgoń <ssevve@gmail.com>
2024-01-02 15:53:27 +0100

    Add more inline padding

--- client/src/features/businesses/components/BusinessesErrorMessage/BusinessesErrorMessage.tsx
+++ client/src/features/businesses/components/BusinessesErrorMessage/BusinessesErrorMessage.tsx
@@ -8,7 +8,7 @@ interface BusinessesErrorMessageProps {
 
 export function BusinessesErrorMessage({ message }: BusinessesErrorMessageProps) {
   return (
-    <Box textAlign="center" paddingInline={1} paddingBlock={2}>
+    <Box textAlign="center" paddingInline={2} paddingBlock={2}>
       <DomainDisabledIcon sx={{ fontSize: 80, mb: 2, opacity: 0.4 }} />
       <Typography>{message}</Typography>
     </Box>
