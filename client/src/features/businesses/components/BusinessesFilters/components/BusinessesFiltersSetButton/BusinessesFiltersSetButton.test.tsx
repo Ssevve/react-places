391daabb700c756a739c11e22006578dc06c91a8
Seweryn Świrgoń <ssevve@gmail.com>
2024-01-09 16:18:55 +0100

    Separate setting and closing filters in BusinessesFiltersSetButton

--- client/src/features/businesses/components/BusinessesFilters/components/BusinessesFiltersSetButton/BusinessesFiltersSetButton.test.tsx
+++ client/src/features/businesses/components/BusinessesFilters/components/BusinessesFiltersSetButton/BusinessesFiltersSetButton.test.tsx
@@ -3,13 +3,17 @@ import { userEvent } from '@testing-library/user-event';
 import { BusinessesFiltersSetButton } from './BusinessesFiltersSetButton';
 
 describe('BusinessesFiltersSetButton', () => {
-  it('should call "setFilters" prop on click', async () => {
+  it('should call "setFilters" and "closeFilters" props on click', async () => {
     const user = userEvent.setup();
     const mockSetFilters = vi.fn();
+    const mockCloseFilters = vi.fn();
 
-    render(<BusinessesFiltersSetButton setFilters={mockSetFilters} />);
+    render(
+      <BusinessesFiltersSetButton setFilters={mockSetFilters} closeFilters={mockCloseFilters} />,
+    );
     await user.click(screen.getByRole('button'));
 
     expect(mockSetFilters).toHaveBeenCalledOnce();
+    expect(mockCloseFilters).toHaveBeenCalledOnce();
   });
 });
--- client/src/features/businesses/components/BusinessesFilters/components/BusinessesFiltersSetButton/BusinessesFiltersSetButton.tsx
+++ client/src/features/businesses/components/BusinessesFilters/components/BusinessesFiltersSetButton/BusinessesFiltersSetButton.tsx
@@ -2,11 +2,18 @@ import Button from '@mui/material/Button';
 
 interface FiltersToggleProps {
   setFilters: () => void;
+  closeFilters: () => void;
 }
 
-export function BusinessesFiltersSetButton({ setFilters }: FiltersToggleProps) {
+export function BusinessesFiltersSetButton({ setFilters, closeFilters }: FiltersToggleProps) {
   return (
-    <Button variant="contained" onClick={setFilters}>
+    <Button
+      variant="contained"
+      onClick={() => {
+        setFilters();
+        closeFilters();
+      }}
+    >
       Set Filters
     </Button>
   );
