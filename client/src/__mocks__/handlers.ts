883dc6fcaff3ebd1ff93ba47fa41b309509e374b
Seweryn Świrgoń <ssevve@gmail.com>
2024-01-09 18:32:28 +0100

    Update msw handler url

--- client/src/__mocks__/handlers.ts
+++ client/src/__mocks__/handlers.ts
@@ -2,5 +2,7 @@ import { http, HttpResponse } from 'msw';
 import { mockYelpBusinessesResponse } from './data';
 
 export const handlers = [
-  http.get('http://localhost:5000/yelp', () => HttpResponse.json(mockYelpBusinessesResponse)),
+  http.get('http://localhost:5000/yelp/businesses', () =>
+    HttpResponse.json(mockYelpBusinessesResponse),
+  ),
 ];
