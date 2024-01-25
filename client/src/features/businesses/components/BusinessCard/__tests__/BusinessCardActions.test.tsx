cc124161e56887f39530603e56304f500bfc9030
Seweryn Świrgoń <ssevve@gmail.com>
2024-01-25 18:11:57 +0100

    Add BusinessCardActions tests

--- client/src/features/businesses/components/BusinessCard/__tests__/BusinessCardActions.test.tsx
+++ client/src/features/businesses/components/BusinessCard/__tests__/BusinessCardActions.test.tsx
@@ -1,9 +1,33 @@
-// it('should render button to center the business on the map', async () => {
-//   renderBusinessCard();
-//   expect(screen.getByRole('button', { name: /show on map/i })).toBeInTheDocument();
-// });
+import { render, screen } from '@/tests/utils';
+import { BusinessCardActions, BusinessCardActionsProps } from '../BusinessCardActions';
 
-// it('should render Yelp link for the business', () => {
-//   renderBusinessCard();
-//   expect(screen.getByRole('link')).toHaveAttribute('href', business.url);
-// });
+const renderBusinessCardActions = (props?: Partial<BusinessCardActionsProps>) => {
+  return render(
+    <BusinessCardActions
+      isContactExpanded={false}
+      name="Test"
+      toggleExpandedContact={() => {}}
+      yelpUrl="test.com"
+      {...props}
+    />,
+  );
+};
+
+describe('BusinessCardActions', () => {
+  it('should render button to center the business on the map', async () => {
+    renderBusinessCardActions();
+    expect(screen.getByRole('button', { name: /show on map/i })).toBeInTheDocument();
+  });
+
+  it('should render button to expand contact info', () => {
+    const expectedUrl = 'yelp.test.com';
+    renderBusinessCardActions({ yelpUrl: expectedUrl });
+    expect(screen.getByRole('button', { name: /contact/i })).toBeInTheDocument();
+  });
+
+  it('should render Yelp link for the business', () => {
+    const expectedUrl = 'yelp.test.com';
+    renderBusinessCardActions({ yelpUrl: expectedUrl });
+    expect(screen.getByRole('link')).toHaveAttribute('href', expectedUrl);
+  });
+});
