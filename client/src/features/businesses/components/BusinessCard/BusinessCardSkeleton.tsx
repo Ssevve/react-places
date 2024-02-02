73ce7fbf4ff4b63190fd27accdbeba7b89e1c6f4
Seweryn Świrgoń <ssevve@gmail.com>
2024-02-02 16:46:04 +0100

    Fix broken skeleton width on mobile screens

--- client/src/features/businesses/components/BusinessCard/BusinessCardSkeleton.tsx
+++ client/src/features/businesses/components/BusinessCard/BusinessCardSkeleton.tsx
@@ -7,7 +7,7 @@ export function BusinessCardSkeleton() {
   const isSmallMobile = useMediaQuery(theme.breakpoints.down('sm'));
   return (
     <Box padding={2} display="flex" flexDirection={isSmallMobile ? 'column' : 'row'} gap={2}>
-      <Skeleton width={isSmallMobile ? 1 : 96} height={96} variant="rounded" />
+      <Skeleton width={isSmallMobile ? '100%' : 96} height={96} variant="rounded" />
       <Box display="flex" flexDirection="column" gap={1}>
         <Skeleton width={175} />
         <Skeleton width={100} />
