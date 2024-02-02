2168b9df0511b1309650a1cd643302e79effe8c4
Seweryn Świrgoń <ssevve@gmail.com>
2024-02-02 16:55:49 +0100

    Add api urls to env vars. Stricter filtering of cities results;

--- server/config/env.ts
+++ server/config/env.ts
@@ -5,6 +5,8 @@ const envSchema = z.object({
   YELP_CLIENT_ID: z.string(),
   YELP_API_KEY: z.string(),
   API_NINJAS_API_KEY: z.string(),
+  YELP_BUSINESSES_SEARCH_ENDPOINT: z.string(),
+  API_NINJAS_CITY_ENDPOINT: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
