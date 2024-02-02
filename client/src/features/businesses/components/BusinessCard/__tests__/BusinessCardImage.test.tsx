794064d80aa86dc76f2ce546834f8f86f85be61c
Seweryn Świrgoń <ssevve@gmail.com>
2024-02-02 18:18:34 +0100

    Remove redundant eslint comments from test files

--- client/src/features/businesses/components/BusinessCard/__tests__/BusinessCardImage.test.tsx
+++ client/src/features/businesses/components/BusinessCard/__tests__/BusinessCardImage.test.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable testing-library/no-node-access */
 import { render, screen } from '@/tests/utils';
 import { BusinessCardImage, BusinessCardImageProps } from '../BusinessCardImage';
 
