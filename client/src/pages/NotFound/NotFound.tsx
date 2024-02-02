db0e2e67d91aad0313077f772d36d6fe41a0290d
Seweryn Świrgoń <ssevve@gmail.com>
2024-02-02 16:38:11 +0100

    Change named export to default export for Button component

--- client/src/pages/NotFound/NotFound.tsx
+++ client/src/pages/NotFound/NotFound.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@mui/material';
+import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import { Link as RouterLink } from 'react-router-dom';
@@ -22,12 +22,7 @@ export function NotFound() {
       <Typography marginBlock={4} fontWeight={500}>
         We don't have that page, but we can help you find something near you to visit.
       </Typography>
-      <Button
-        component={RouterLink}
-        to="/"
-        variant="contained"
-        sx={{ mx: 'auto', width: 'fit-content' }}
-      >
+      <Button component={RouterLink} to="/" variant="contained" sx={{ mx: 'auto', width: 'fit-content' }}>
         Home page
       </Button>
     </Box>
